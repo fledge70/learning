0f89d71841443183634012012b4b3c824651d145
fledge70 <llodel3@gmail.com>
2017-07-27 10:08:08 -0500

    Refactored to use PIXI Application class
    Refactored to use PIXI Application class, changed screen orientation to portrait, and changed to improved FPS counter.

--- jump/js/jump.js
+++ jump/js/jump.js
@@ -1,23 +1,19 @@
 // Helpful Aliases
-var Container = PIXI.Container,
-  autoDetectRenderer = PIXI.autoDetectRenderer,
-  loader = PIXI.loader,
+var loader = PIXI.loader,
   resources = PIXI.loader.resources,
   Sprite = PIXI.Sprite;
 
-// Create the PIXI stage and renderer
-var stage = new Container(),
-  renderer = autoDetectRenderer(640, 360);
-document.body.appendChild(renderer.view);
+// Create the PIXI app (helper class that includes
+// necessary elements like stage and renderer)
+var app = new PIXI.Application(360, 640);
+document.body.appendChild(app.view);
 
 // Define global variables
 var player,
   gameState,
   jumping,
-  fpsCounter,
   fpsText,
-  currentTime,
-  lastTime;
+  fpsDisplayCounter = 0;
 
 // Load assets
 loader
@@ -39,44 +35,29 @@ function setup() {
   fpsText = new PIXI.Text('FPS:', {fill: '#00ff99'});
   fpsText.x = 16;
   fpsText.y = 16;
-  stage.addChild(fpsText);
+  app.stage.addChild(fpsText);
 	
   // Set initial game state
   gameState = play;
 
   // Set initial position
-  player.x = renderer.width / 2 - player.width / 2;
-  player.y = renderer.height - player.height;
+  player.x = app.renderer.width / 2 - player.width / 2;
+  player.y = app.renderer.height - player.height;
   // Set initial velocity and jumping state
   player.vx = 0;
   player.vy = 0;
   jumping = false;
 
 
-  stage.addChild(player);
+  app.stage.addChild(player);
 
   // Make the stage interactive to capture pointer events
-  stage.interactive = true;
-  stage.hitArea = new PIXI.Rectangle(0, 0, 640, 360);
-  stage.on('pointerdown', onClick);
+  app.stage.interactive = true;
+  app.stage.hitArea = new PIXI.Rectangle(0, 0, 360, 640);
+  app.stage.on('pointerdown', onClick);
 
-  // Initilize counter used for FPS
-  lastTime = (new Date()).getMilliseconds();
-
-  // Start the game loop
-  gameLoop();
-}
-
-function gameLoop() {
-  
-  requestAnimationFrame(gameLoop);
-  
-  // Update the current game gameState
-  gameState();
-  
-  // Draw
-
-  renderer.render(stage);
+  // Start the listener to update on every ticker from App
+  app.ticker.add(gameState);
 }
 
 function play() {
@@ -92,10 +73,13 @@ function play() {
   }
 
   // Constrain player to bounds of game screen
-  fixToBounds(player, 0, 0, renderer.width, renderer.height);
+  fixToBounds(player, 0, 0, app.renderer.width, app.renderer.height);
 
-  currentTime = (new Date()).getMilliseconds();
-  fpsCounter = 1000 / (currentTime - lastTime);
-  fpsText.text = "FPS: " + Math.round(fpsCounter);
-  lastTime = (new Date()).getMilliseconds();
+  // Update FPS Counter text once every
+  // 30 ticks (approx twice per second)
+  fpsDisplayCounter++
+  if (fpsDisplayCounter >= 30) {
+    fpsText.text = "FPS: " + Math.round(app.ticker.FPS);
+    fpsDisplayCounter = 0;
+  }
 }
