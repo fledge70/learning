a9d47643952aee2b0f60f0699206c73559da88a6
fledge70 <llodel3@gmail.com>
2017-07-28 16:18:35 -0500

    Incorporated simple game states

--- jump/js/jump.js
+++ jump/js/jump.js
@@ -13,35 +13,87 @@ var player,
   treeLeft,
   treeRight,
   diamondPool = [],
+  playScene,
+  gameOverScene,
   gameState,
   fpsText,
   fpsDisplayCounter = 0,
+  gameOverMessage,
   score = 0,
+  jumpCharging = false,
+  jumpCharge = 0,
+  jumpMeterBar,
+  jumpMeterMask,
+  messageBox,
   scoreText,
   slipSpeed = 1.5,
   termVelocity = 6;
 
 // Load assets
 loader
-  .add('img/diamond.png')
-  .add('img/cat.png')
-  .add('img/tree.png')
+  .add(['img/diamond.png',
+  'img/cat.png',
+  'img/tree.png',
+  'img/bar.png',
+  'img/mask.png',
+  'img/messagebox.png'])
   .load(setup);
 
-function onClick() {
+function onPointerDown() {
   if (player.x === treeLeft.width) {
-    // jumping = true;
-    player.vx = 4;
-    player.vy = -4;
+    jumpCharging = true;
   } else if (player.x === app.renderer.width - treeRight.width
     - player.width) {
-    player.vx = -4;
-    player.vy = -4;
+    jumpCharging = true;
   }
 }
 
+function onPointerUp() {
+  if (jumpCharging) {
+    jumpCharging = false;
+  }
+  makeJump();
+}
+
+function onPointerOut() {
+  if (jumpCharging) {
+    jumpCharging = false;
+  }
+  makeJump();
+}
+
+function onPointerTap() {
+  console.log("onPointerTap executed!");
+  window.location.reload(false);
+}
+
+function makeJump() {
+  jumpCharging = false;
+  if (player.x === treeLeft.width) {
+    player.vx = 0.035 * jumpCharge;
+    player.vy = -0.035 * jumpCharge;
+  } else if (player.x === app.renderer.width - treeRight.width
+    - player.width) {
+    player.vx = -0.035 * jumpCharge;
+    player.vy = -0.035 * jumpCharge;
+  }
+  jumpCharge = 0;
+
+  // Update Jump Charge Bar
+  jumpMeterMask.width = 200 - jumpCharge;
+  jumpMeterMask.x = jumpMeterBar.x + 200 -jumpMeterMask.width;
+}
+
 function setup() {
-	// Create sprites
+	// Create containers for different game scenes
+  playScene = new PIXI.DisplayObjectContainer();
+  gameOverScene = new PIXI.DisplayObjectContainer();
+  playScene.visible = true;
+  gameOverScene.visible = false;
+  app.stage.addChild(playScene);
+  app.stage.addChild(gameOverScene);
+
+  // Create sprites
   player = new Sprite(resources["img/cat.png"].texture);
   treeLeft = new Sprite(resources['img/tree.png'].texture);
   treeRight = new Sprite(resources['img/tree.png'].texture);
@@ -68,30 +120,61 @@ function setup() {
     diamondPool.push(theDiamond);
   }
 
-  // Add all sprites to the stage
-  app.stage.addChild(player);
-  app.stage.addChild(treeLeft);
-  app.stage.addChild(treeRight);
+  // Create the JumpCharge bar display
+  jumpMeterBar = new Sprite(resources['img/bar.png'].texture);
+  jumpMeterBar.height = 16;
+  jumpMeterBar.width = 200;
+  jumpMeterBar.x = app.renderer.width / 2 - jumpMeterBar.width / 2;
+  jumpMeterBar.y = 16;
+  playScene.addChild(jumpMeterBar);
+  // Create the JumpCharge Mask
+  jumpMeterMask = new Sprite(resources['img/mask.png'].texture);
+  jumpMeterMask.height = jumpMeterBar.height;
+  jumpMeterMask.width = 200 - jumpCharge;
+  jumpMeterMask.x = jumpMeterBar.x + 200 -jumpMeterMask.width;
+  jumpMeterMask.y = jumpMeterBar.y;
+  playScene.addChild(jumpMeterMask);
+
+  // Add all sprites to the playScene stage
+  playScene.addChild(player);
+  playScene.addChild(treeLeft);
+  playScene.addChild(treeRight);
   for (var i = 0; i < diamondPool.length; i++) {
-    app.stage.addChild(diamondPool[i]);
+    playScene.addChild(diamondPool[i]);
   }
 
   // Create text for FPS counter display
   fpsText = new PIXI.Text('FPS:', {fill: '#00ff99'});
   fpsText.x = app.renderer.width - 104;
   fpsText.y = app.renderer.height - 32;
-  app.stage.addChild(fpsText);
+  playScene.addChild(fpsText);
 
   // Create score display
   scoreText = new PIXI.Text('Score: ' + score, {fill: '#00ff99'});
   scoreText.x = 16;
   scoreText.y = app.renderer.height - 32;
-  app.stage.addChild(scoreText);
+  playScene.addChild(scoreText);
 
   // Make the stage interactive to capture pointer events
   app.stage.interactive = true;
   app.stage.hitArea = new PIXI.Rectangle(0, 0, 352, 640);
-  app.stage.on('pointerdown', onClick);
+  app.stage.on('pointerdown', onPointerDown);
+  app.stage.on('pointerup', onPointerUp);
+  app.stage.on('pointerout', onPointerOut);
+
+  // Set up the gameOver display items
+  messageBox = new Sprite(resources['img/messagebox.png'].texture);
+  messageBox.x = 64;
+  messageBox.y = 64;
+  messageBox.width = app.renderer.width - 128;
+  messageBox.height = 256;
+  gameOverScene.addChild(messageBox);
+  // Set up gameOver message
+  gameOverMessage = new PIXI.Text('You win!\nClick or tap\nto reload.',
+    {fill: '#bbbbbb'});
+  gameOverMessage.x = messageBox.x + 8;
+  gameOverMessage.y = messageBox.y + 8;
+  gameOverScene.addChild(gameOverMessage);
 
   // Set initial game state
   gameState = play;
@@ -104,6 +187,14 @@ function play() {
   // Apply velocity values
   player.x += player.vx;
   player.y += player.vy;
+
+  // Charge the jumping if pointer held down
+  if (jumpCharging) {
+    jumpCharge += 3;
+    if (jumpCharge > 200) jumpCharge = 200;
+    jumpMeterMask.width = 200 - jumpCharge;
+    jumpMeterMask.x = jumpMeterBar.x + 200 -jumpMeterMask.width;
+  }
 
   // Constrain player to bounds of game screen and trees
   fixToBounds(player, treeLeft.width, 0,
@@ -125,17 +216,27 @@ function play() {
     }
   }
 
-  // TODO: check for collision between player and diamonds
   for (var i = 0; i < diamondPool.length; i++) {
     if (spritesCollide(player, diamondPool[i])) {
       // destroy the diamond touched
-      app.stage.removeChild(diamondPool[i]);
+      playScene.removeChild(diamondPool[i]);
       diamondPool.splice(i, 1);
       
       // Update score and associated text
       score += 25;
       scoreText.text = 'Score: ' + score;
     }
+  }
+
+  // Check for game over condition - that all diamonds
+  // have been collected.  If true, switch to game over state
+  if (diamondPool.length === 0) {
+    gameOverScene.visible = true;
+    app.stage.off('pointerdown', onPointerDown);
+    app.stage.on('pointertap', onPointerTap);
+    app.ticker.remove(gameState);    
+    gameState = gameOver;
+    app.ticker.add(gameState);
   }
 
   // Update FPS Counter text once every
@@ -145,4 +246,8 @@ function play() {
     fpsText.text = "FPS: " + Math.round(app.ticker.FPS);
     fpsDisplayCounter = 0;
   }
+}
+
+function gameOver() {
+  // Seems like this doesn't need anything to happen here
 }
